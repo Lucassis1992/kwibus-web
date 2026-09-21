37afb358beaa9bdbc83144e8ec616e98e5d0e3ae
Lucas <lucasknijn1@gmail.com>
2026-09-21 20:37:56 +0200

    Web build voor kwibus.online (root base-href) + vercel.json

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2578724682" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1929472811" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
